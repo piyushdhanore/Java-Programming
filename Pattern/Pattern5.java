f7bb5b06f3b517f6eec3abcf0c35758f4ae0cbc8
piyushdhanore <piyushdhanore14@gmail.com>
2024-01-05 18:36:56 +0530

    Pattern5 - Swastik

--- Pattern/Pattern5.java
+++ Pattern/Pattern5.java
@@ -10,7 +10,7 @@ public class Pattern5
                 if (i==n/2 || j==n/2 || i==0 && j>n/2 || j==n-1 && i>n/2 || i==n-1 && j<n/2 || j==0 && i<n/2) 
                 {
                    System.out.print("*"+ " "); 
-                } 
+                }
                 else 
                 {
                    System.out.print(" "+" "); 
