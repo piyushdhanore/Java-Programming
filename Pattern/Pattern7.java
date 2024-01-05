a66d3f0a5fc973ae47172028c03b7761283a597c
piyushdhanore <piyushdhanore14@gmail.com>
2024-01-05 19:15:37 +0530

    Pattern7 - Reverse Right angled triangle

--- Pattern/Pattern7.java
+++ Pattern/Pattern7.java
@@ -1,3 +1,15 @@
-public class Pattern7 {
-    
+public class Pattern7 
+{
+    public static void main(String[] args) 
+    {
+        int n=7;
+        for(int i=0; i<7; i++)
+        {
+            for(int j=0;j<n-i;j++)
+            {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }    
 }
