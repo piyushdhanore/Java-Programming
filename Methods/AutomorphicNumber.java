5530399c40c57b6b21386d5f8585a74da68a3281
piyushdhanore <piyushdhanore14@gmail.com>
2024-01-21 19:30:16 +0530

    AUto

--- Methods/AutomorphicNumber.java
+++ Methods/AutomorphicNumber.java
@@ -4,7 +4,17 @@ public class AutomorphicNumber
     public static void main(String[] args) 
     {
         Scanner an = new Scanner(System.in);
-        
+        System.out.println("Enter a number: ");
+        int ip = an.nextInt();
+        int den = power(10,count(ip));
+        if ((ip*ip)%den==ip)
+        {
+           System.out.println("it is an automorphic number"); 
+        } 
+        else 
+        {
+            System.out.println("it is not an automorphic number");
+        }
         an.close();
     }
 
@@ -17,5 +27,15 @@ public class AutomorphicNumber
             num/=10;    
         }
         return count;
+    }
+
+    public static int power(int base, int raise)
+    {
+        int power = 1;
+        for (int i = 0; i < raise; i++) 
+        {
+            power = power * base;    
+        }
+        return power;
     }
 }
