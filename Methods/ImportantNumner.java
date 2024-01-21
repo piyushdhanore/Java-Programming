3d321900921ebdcf9a854597a1b0b1a48eb1d753
piyushdhanore <piyushdhanore14@gmail.com>
2024-01-21 22:08:54 +0530

    important

--- Methods/ImportantNumner.java
+++ Methods/ImportantNumner.java
@@ -5,5 +5,35 @@ public class ImportantNumner
     {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter a number: ");
+        int ip = in.nextInt();
+
+        while (ip>1) 
+        {
+            if (ip%2==0) 
+            {
+                ip/=2;
+            } 
+            else if (ip%3==0) 
+            {
+                ip/=3;
+            } 
+            else if (ip%5==0) 
+            {
+                ip/=5;
+            }
+            else
+            {
+                break;
+            }      
+        }
+        if (ip==2) 
+        {
+            System.out.println("it is an important number");
+        } 
+        else 
+        {
+            System.out.println("it is not an important number");
+        }
+
     }
 }
