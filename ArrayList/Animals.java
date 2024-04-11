1cf949fb35dc75ac33985ff341204d02572b051a
piyushdhanore <piyushdhanore14@gmail.com>
2024-04-11 23:22:02 +0530

    Creation of two separate Objects in ArrayList

---
+++ ArrayList/Animals.java
@@ -0,0 +1,35 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animals {
+
+	public static void main(String[] args) {
+		
+		List<String> a1 = new ArrayList<String>();
+		
+		a1.add("Dog");
+		a1.add("Cat");
+		a1.add("Tiger");
+		a1.add("Mouse");
+		a1.add("Spider");
+		a1.add("Crocodile");
+		a1.add("Giraffe");
+		
+		System.out.println(a1);
+		
+		List<String> a2 = new ArrayList<String>();
+		
+		a2.add("Lion");
+		a2.add("Snake");
+		a2.add("HoneyBee");
+		a2.add("Tarantula");
+		a2.add("Eagle");
+		a2.add("Hippo");
+		a2.add("Turtle");
+		
+		System.out.println(a2);
+	}
+
+}
