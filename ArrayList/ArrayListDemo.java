f79d516f27f43c6e466186648fc2118755b9647f
piyushdhanore <piyushdhanore14@gmail.com>
2024-04-11 20:09:53 +0530

    methods of arraylist

---
+++ ArrayList/ArrayListDemo.java
@@ -0,0 +1,19 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		List<String> str = new ArrayList<String>();
+		
+		str.add(null);
+		str.add("The Simpsons");
+		
+		System.out.println(str);
+
+	}
+
+}
